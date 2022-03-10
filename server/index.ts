ef29e882a2f6b85800d3704bcfbf7a1728087484
Zac Jones <zacjones93@gmail.com>
2022-03-10 15:38:01 -0500

    allow cors

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,12 @@ const app = express()
 
 app.use(express.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/products', async (req, res) => {
     const { searchString, take, skip } = req.query
 
